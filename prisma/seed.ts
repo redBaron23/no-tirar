ebb82cbcabbb1237497d516e8970404096416ab6
Pato <23110463+redBaron23@users.noreply.github.com>
2024-06-20 18:47:46 -0300

    add prisma adapter

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,25 +3,23 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const existingUser = await prisma.user.findUnique({
-    where: { id: "0" },
-  });
-
-  if (!existingUser) {
-    const botUser = await prisma.user.upsert({
-      where: { email: "bot@example.com" },
-      update: {},
-      create: {
-        id: "0",
-        email: "bot@grammarguardian.ar",
-        name: "Grammar Guardian.ar",
-      },
-    });
-
-    console.log({ botUser });
-  } else {
-    console.log("User with ID '0' already exists.");
-  }
+  // const existingUser = await prisma.user.findUnique({
+  //   where: { id: "0" },
+  // });
+  // if (!existingUser) {
+  //   const botUser = await prisma.user.upsert({
+  //     where: { email: "bot@example.com" },
+  //     update: {},
+  //     create: {
+  //       id: "0",
+  //       email: "bot@grammarguardian.ar",
+  //       name: "Grammar Guardian.ar",
+  //     },
+  //   });
+  //   console.log({ botUser });
+  // } else {
+  //   console.log("User with ID '0' already exists.");
+  // }
 }
 
 main()
