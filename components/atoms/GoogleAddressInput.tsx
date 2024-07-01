9445c00e6e222ebc32a292f29de48ccfa77e1ae9
Pato <23110463+redBaron23@users.noreply.github.com>
2024-07-01 20:01:39 -0300

    add google step

--- components/atoms/GoogleAddressInput.tsx
+++ components/atoms/GoogleAddressInput.tsx
@@ -1,14 +1,48 @@
-"use client";
+import { useState, useRef } from "react";
+import { Autocomplete } from "@react-google-maps/api";
+import { Input } from "../ui/input";
 
-import AutoComplete from "react-google-autocomplete";
+// This must be called in parent tree
+
+// const { isLoaded, loadError } = useLoadScript({
+//   googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_PLACES_API_KEY!,
+//   libraries,
+// });
 
 const GoogleAddressInput = () => {
-  console.log(process.env.NEXT_PUBLIC_GOOGLE_PLACES_API_KEY);
+  const [address, setAddress] = useState<string>("");
+  const autocompleteRef = useRef<google.maps.places.Autocomplete | null>(null);
+
+  const onPlaceChanged = () => {
+    if (autocompleteRef.current !== null) {
+      const place = autocompleteRef.current.getPlace();
+      if (place.formatted_address) {
+        setAddress(place.formatted_address);
+      }
+    }
+  };
+
+  const onLoad = (autoC: google.maps.places.Autocomplete) => {
+    autocompleteRef.current = autoC;
+  };
+
   return (
-    <AutoComplete
-      apiKey={process.env.NEXT_PUBLIC_GOOGLE_PLACES_API_KEY}
-      onPlaceSelected={(place) => console.log(place)}
-    />
+    <div className="relative">
+      <Autocomplete
+        onLoad={onLoad}
+        onPlaceChanged={onPlaceChanged}
+        options={{
+          componentRestrictions: { country: "AR" }, // Restrict results to Argentina
+        }}
+      >
+        <Input
+          type="text"
+          placeholder="Enter your address"
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+        />
+      </Autocomplete>
+    </div>
   );
 };
 
--- components/molecules/GeneralInfoStep.tsx
+++ components/molecules/GeneralInfoStep.tsx
@@ -1,68 +1,72 @@
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
-import { MapPinIcon, ImageIcon } from "lucide-react";
+import { ImageIcon } from "lucide-react";
 import GoogleAddressInput from "@/components/atoms/GoogleAddressInput";
+import { Libraries, useLoadScript } from "@react-google-maps/api";
+import { Skeleton } from "../ui/skeleton";
+
+const libraries: Libraries = ["places"];
 
 const GeneralInfoStep = () => {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_PLACES_API_KEY!,
+    libraries,
+  });
+
   return (
-    <div className="bg-secondary my-4 flex h-full w-full max-w-3xl flex-col items-center justify-center rounded-md border p-4 text-gray-600">
-      <form className="grid w-full grid-cols-1 gap-8">
-        <div className="grid gap-6 md:grid-cols-2">
-          <div className="grid gap-2">
-            <Label className="text-gray-700" htmlFor="name">
-              Nombre del Establecimiento
-            </Label>
-            <Input
-              id="name"
-              placeholder="Ingresa el nombre de tu establecimiento"
-            />
-          </div>
-          <div className="grid gap-2">
-            <Label className="text-gray-700" htmlFor="address">
-              Dirección
-            </Label>
-            <div className="relative">
-              {/* <MapPinIcon className="text-muted-foreground absolute left-2.5 top-2.5 h-4 w-4" />
-              <Input
-                id="address"
-                placeholder="Ingresa la dirección de tu establecimiento"
-                className="pl-8"
-              /> */}
+    <div className="my-4 flex h-full w-full max-w-3xl flex-col items-center justify-center rounded-md border bg-secondary p-4 text-gray-600">
+      {isLoaded ? (
+        <form className="grid w-full grid-cols-1 gap-8">
+          <div className="grid gap-6 md:grid-cols-2">
+            <div className="grid gap-2">
+              <Label className="text-gray-700" htmlFor="name">
+                Nombre del Establecimiento
+              </Label>
+              <Input id="name" placeholder="Ingresa el nombre" />
             </div>
-            <GoogleAddressInput />
-            <div className="text-muted-foreground text-sm">
-              Haz clic en el ícono del mapa para seleccionar tu ubicación.
+            <div className="grid gap-2">
+              <Label className="text-gray-700" htmlFor="address">
+                Dirección
+              </Label>
+              <GoogleAddressInput />
             </div>
           </div>
-        </div>
-        <div className="grid gap-6 md:grid-cols-2">
-          <div className="grid gap-2">
-            <Label className="text-gray-700" htmlFor="profile-photo">
-              Foto de Perfil
-            </Label>
-            <label htmlFor="profile-upload" className="cursor-pointer">
-              <div className="relative flex h-16 w-16 items-center justify-center overflow-hidden rounded-full bg-gray-200">
-                <ImageIcon className="h-8 w-8 text-gray-500" />
-              </div>
-              <input id="profile-upload" type="file" className="hidden" />
-            </label>
+          <div className="grid gap-6 md:grid-cols-2">
+            <div className="grid gap-2">
+              <Label className="text-gray-700" htmlFor="profile-photo">
+                Foto de Perfil
+              </Label>
+              <label htmlFor="profile-upload" className="cursor-pointer">
+                <div className="relative flex h-16 w-16 items-center justify-center overflow-hidden rounded-full bg-gray-200">
+                  <ImageIcon className="h-8 w-8 text-gray-500" />
+                </div>
+                <input id="profile-upload" type="file" className="hidden" />
+              </label>
+            </div>
+            <div className="grid gap-2">
+              <Label className="text-gray-700" htmlFor="background-photo">
+                Foto de Fondo
+              </Label>
+              <label
+                htmlFor="background-upload"
+                className="w-full cursor-pointer"
+              >
+                <div className="relative flex h-16 w-full items-center justify-center overflow-hidden rounded-md bg-gray-200">
+                  <ImageIcon className="h-8 w-8 text-gray-500" />
+                </div>
+                <input id="background-upload" type="file" className="hidden" />
+              </label>
+            </div>
           </div>
-          <div className="grid gap-2">
-            <Label className="text-gray-700" htmlFor="background-photo">
-              Foto de Fondo
-            </Label>
-            <label
-              htmlFor="background-upload"
-              className="w-full cursor-pointer"
-            >
-              <div className="relative flex h-16 w-full items-center justify-center overflow-hidden rounded-md bg-gray-200">
-                <ImageIcon className="h-8 w-8 text-gray-500" />
-              </div>
-              <input id="background-upload" type="file" className="hidden" />
-            </label>
-          </div>
+        </form>
+      ) : (
+        <div className="flex w-full flex-col gap-3">
+          <Skeleton className="h-8 w-1/4 rounded-xl" />
+          <Skeleton className="h-8 w-full rounded-xl" />
+          <Skeleton className="h-8 w-1/4 rounded-xl" />
+          <Skeleton className="h-8 w-full rounded-xl" />
         </div>
-      </form>
+      )}
     </div>
   );
 };
