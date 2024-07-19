09ac9355113c1d9f0c8bf261b5aeea67ccb97765
Pato <23110463+redBaron23@users.noreply.github.com>
2024-07-19 02:57:27 -0300

    map

--- components/organisms/RestaurantCardList.tsx
+++ components/organisms/RestaurantCardList.tsx
@@ -6,6 +6,10 @@ interface RestaurantCardListProps {
 }
 
 const RestaurantCardList = ({ restaurants }: RestaurantCardListProps) => {
+  if (restaurants.length) {
+    return "No hay restaurants carga2";
+  }
+
   return (
     <div className="grid grid-cols-1 gap-6 p-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
       {restaurants.map((restaurant) => (
