24158e138a343379bba43cde0aeff48e76fbb3b2
Pato <23110463+redBaron23@users.noreply.github.com>
2024-08-11 12:32:20 -0300

    placeholder

--- components/templates/RestaurantsMap.tsx
+++ components/templates/RestaurantsMap.tsx
@@ -1,8 +1,16 @@
 import { Restaurant } from "@prisma/client";
 import dynamic from "next/dynamic";
+import { Skeleton } from "../ui/skeleton";
 
 const ClientMap = dynamic(() => import("@/components/organisms/ClientMap"), {
-  loading: () => <p>A map is loading</p>,
+  loading: () => (
+    <div className="fixed inset-0 flex h-screen w-screen items-center justify-center bg-teal-500">
+      <Skeleton className="absolute inset-0 h-full w-full animate-pulse bg-white/10" />
+      <span className="animate-pulse text-2xl font-bold text-white">
+        Cargando...
+      </span>
+    </div>
+  ),
   ssr: false,
 });
 
